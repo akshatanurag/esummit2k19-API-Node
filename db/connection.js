cbe777dd6399e6c8f3a0a4a5c6bbf75286f90ba1
Akshat Anurag <akshatanurag1998@gmail.com>
2018-12-01 16:01:01 +0530

    cloud db

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/e-summit", {
+mongoose.connect("mongodb://root:kW8Qc7gBT@ds123434.mlab.com:23434/esummit", {
     useNewUrlParser: true
 }, () => {
-    //console.log("connected to db");
+    console.log("connected to db");
 });
 mongoose.Promise = global.Promise;
 
