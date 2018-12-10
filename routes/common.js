61716d13be0cdef4bb9ffd6baa57084fa9c30049
Akshat Anurag <akshatanurag1998@gmail.com>
2018-12-10 08:10:50 +0530

    finding client's ip

--- routes/common.js
+++ routes/common.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
+const requestIp = require('request-ip');
 
 const router = express.Router();
 
+const ipMiddleware = function(req, res) {
+    const clientIp = requestIp.getClientIp(req);
+     console.log(clientIp)
+};
+
 router.get("/", (req, res) => {
+    ipMiddleware(req,res);
     return res.send({
         esummit_api: "v 1.0"
     })
