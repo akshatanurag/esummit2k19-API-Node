11473381e2906eb4b96bf2c662cc09b84a7f569e
Akshat Anurag <akshatanurag1998@gmail.com>
2019-02-10 15:10:04 +0530

    db

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const log = require('../config/bunyan-config')
 // mongodb://localhost/e-summit
 //mongodb://techies:techiescodefastaf%23tJ5qF6rBCK%242019@13.127.68.61:27017/esummit
-    mongoose.connect("mongodb://techies:newTechiesCodeFastAfin2019#!@127.0.0.1:27017/esummit", {
+    mongoose.connect("mongodb://techies:newTechiesCodeFastAfin2019#!@139.59.81.217:27017/esummit", {
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true 
