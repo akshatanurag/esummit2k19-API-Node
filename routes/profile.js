7a2e80a7f08049c0c459beb2453693b184e5ad38
Akshat Anurag <akshatanurag1998@gmail.com>
2019-03-07 11:22:30 +0530

    fixed other school registration

--- routes/profile.js
+++ routes/profile.js
@@ -22,7 +22,7 @@ const sanitizer = require('sanitizer');
 
 router.post(
   '/profile',
-  [middleware.isLoggedIn, middleware.isVerified],
+  [middleware.isLoggedIn, middleware.isVerified, middleware.isFromKiit],
   async (req, res) => {
     try {
       let findUser = await User.findOne({
@@ -149,7 +149,7 @@ router.post(
         //Fix needed
         // Send mail to kiit student
         if (profile.uni == "kiit university") {
-          // if (req.kiit) {
+          if (req.kiit) {
             let verifyToken = randomstring.generate({
               length: 50,
               charset: 'hex'
@@ -172,12 +172,12 @@ router.post(
               });
             }
 
-          // } else {
-          //   return res.status(400).send({
-          //     success: false,
-          //     message: "Incorrect KIIT mail id"
-          //   })
-          // }
+          } else {
+            return res.status(400).send({
+              success: false,
+              message: "Incorrect KIIT mail id"
+            })
+          }
 
         } else {
           await profile.save();
@@ -206,7 +206,7 @@ router.post(
   }
 );
 
-router.get("/profile",[middleware.isLoggedIn, middleware.isVerified,middleware.isProfileComplete],async (req,res)=>{
+router.get("/profile",[middleware.isLoggedIn, middleware.isVerified,middleware.isProfileComplete,middleware.isFromKiit],async (req,res)=>{
   let findUser = await User.findOne({
     email: req.user.email
   })
